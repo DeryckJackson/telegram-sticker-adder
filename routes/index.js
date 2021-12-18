3bfa9672d4e4e184af984db296af92b10e3eaa63
Jerax <dgjackon01@gmail.com>
2021-12-18 14:42:45 -0800

    bug fix

--- routes/index.js
+++ routes/index.js
@@ -3,23 +3,23 @@ var express = require('express');
 var router = express.Router();
 const axios = require('axios');
 
-axios.defaults.baseUrl = `https://api.telegram.org/bot${process.env.BOT_TOKEN}`;
+axios.defaults.baseURL = `https://api.telegram.org/bot${process.env.BOT_TOKEN}`;
 
 router.post(`/${process.env.BOT_TOKEN}`, async (req, res, next) => {
   try {
     const userId = req.body.result[0].message.from.id;
 
-    const text = 'Stop messaging me, I\'m not done yet';
+    const text = 'Stop messaging me, I\'m not done yet.';
     const body = {
       chat_id: userId,
       text
     };
 
-    await axios.post('/sendMessage', body);
+    const response = await axios.post('/sendMessage', body);
 
-    res.status(200);
+    res.status(200).send("Ok");
   } catch (err) {
-    next(error);
+    next(err);
   }
 });
 
