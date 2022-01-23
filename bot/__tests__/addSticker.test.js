4dca6b11af4d4f2eb4c0052c1bdc6ce1dbf000ab
Jerax <dgjackon01@gmail.com>
2022-01-22 17:26:47 -0800

    complete addSticker tests

--- bot/__tests__/addSticker.test.js
+++ bot/__tests__/addSticker.test.js
@@ -4,9 +4,20 @@ const c = require('../menuConstants');
 const res = require('../responses');
 
 const mockSendMessage = jest.fn();
+const mockAddSticker = jest.fn();
+const mockGetFile = jest.fn();
+const mockAddStickerPhoto = jest.fn();
+const mockResize = jest.fn();
+
 jest.mock('../stickerBot.js', () => {
   return function () {
-    return { sendMessage: mockSendMessage };
+    return {
+      sendMessage: mockSendMessage,
+      addSticker: mockAddSticker,
+      getFile: mockGetFile,
+      addStickerWithPhoto: mockAddStickerPhoto,
+      resize: mockResize
+    };
   };
 });
 
@@ -61,5 +72,91 @@ describe('addSticker', () => {
 
     expect(user.menuState).toEqual(c.stickerGetEmojis);
     expect(mockSendMessage).toHaveBeenCalledWith(res.getEmojis, mockUser.id);
+  });
+
+  it('should send invalid input message', async () => {
+    mockUser.menuState = c.stickerGetPackName;
+    const message = {
+      text: 'WRONG!'
+    };
+
+    const user = await addSticker(mockUser, message);
+
+    expect(mockSendMessage).toHaveBeenCalledWith(res.invalidInput, mockUser.id);
+  });
+
+  it('should send invalid emoji message', async () => {
+    mockUser.menuState = c.stickerGetEmojis;
+    const message = {
+      text: 'WRONG!'
+    };
+
+    const user = await addSticker(mockUser, message);
+
+    expect(mockSendMessage).toHaveBeenCalledWith(res.invalidEmoji, mockUser.id);
+  });
+
+  it('should set menuState to stickerGetSticker and send getSticker message', async () => {
+    mockUser.menuState = c.stickerGetEmojis;
+    const message = {
+      text: '👍'
+    };
+
+    const user = await addSticker(mockUser, message);
+
+    expect(user.menuState).toEqual(c.stickerGetSticker);
+    expect(mockSendMessage).toHaveBeenCalledWith(res.getSticker, mockUser.id);
+  });
+
+  it('should send invalid input message for stickerGetEmojis state', async () => {
+    mockUser.menuState = c.stickerGetEmojis;
+    const message = {
+      sticker: 'Wait this isnt right'
+    };
+
+    const user = await addSticker(mockUser, message);
+
+    expect(mockSendMessage).toHaveBeenCalledWith(res.invalidInput, mockUser.id);
+  });
+
+  it('should set menuState to stickerGetEmojis and call addSticker', async () => {
+    mockUser.menuState = c.stickerGetSticker;
+    const message = {
+      sticker: {
+        file_id: '12345'
+      }
+    };
+
+    const user = await addSticker(mockUser, message);
+
+    expect(user.menuState).toEqual(c.stickerGetEmojis);
+    expect(mockAddSticker).toHaveBeenCalledWith(mockUser, message.sticker.file_id);
+    expect(mockSendMessage).toHaveBeenCalledWith(res.stickerSuccess, mockUser.id);
+  });
+
+  it('should set menuState to stickerGetEmojis and call addStickerWithPhoto', async () => {
+    mockUser.menuState = c.stickerGetSticker;
+    const message = {
+      photo: [{
+        file_id: '12345'
+      }]
+    };
+
+    const user = await addSticker(mockUser, message);
+
+    expect(user.menuState).toEqual(c.stickerGetEmojis);
+    expect(mockGetFile).toHaveBeenCalledWith(message.photo[0].file_id);
+    expect(mockSendMessage).toHaveBeenCalledWith(res.stickerSuccess, mockUser.id);
+  });
+
+  it('should send invalid input message with invalid menuState', async () => {
+    mockUser.menuState = 'this is wrong';
+    const message = {
+      text: 'someText'
+    };
+
+    const user = await addSticker(mockUser, message);
+
+    expect(mockSendMessage).toHaveBeenCalledWith(res.invalidInput, mockUser.id);
   });
 });
